a02e3030f3e3f027c38b2963426d8b5472fdccb0
MaksKortus11 <adiseba11@gmail.com>
2026-01-19 10:38:41 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,8 @@
-fetch('data/hoenn.json')
-  .then(response => response.json())
-  .then(data => {
-    const container = document.getElementById('city-list');
+window.onload = () => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js')
+      .then(() => console.log('Service Worker registered successfully.'))
+      .catch(err => console.error('Service Worker registration failed:', err));
+  }
+};
 
-    data.cities.forEach(city => {
-      const div = document.createElement('div');
-      div.className = 'city-card';
-      div.innerHTML = `<h2>${city.name}</h2>`;
-      div.onclick = () => {
-        window.location.href = `city.html?id=${city.id}`;
-      };
-      container.appendChild(div);
-    });
-  });
