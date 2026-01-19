525853b4018a29a5947230689e0add213a1f8b90
MaksKortus11 <adiseba11@gmail.com>
2026-01-19 10:47:42 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
+// Rejestracja Service Workera
 window.onload = () => {
+  'use strict';
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js')
-      .then(() => console.log('Service Worker registered successfully.'))
-      .catch(err => console.error('Service Worker registration failed:', err));
+    .then(() => console.log('Service Worker registered successfully.'))
+    .catch((error) => console.error('Service Worker registration failed:', error));
   }
-  // Obsługa dropdown menu
-const citySelect = document.getElementById('citySelect');
-if (citySelect) {
-  citySelect.addEventListener('change', (e) => {
-    const selectedCity = e.target.value;
-    if (selectedCity) {
-      window.location.href = selectedCity; // przeniesienie na podstronę
-    }
-  });
-}
 
+  // Dropdown menu
+  const citySelect = document.getElementById('citySelect');
+  if (citySelect) {
+    citySelect.addEventListener('change', (e) => {
+      const selectedCity = e.target.value;
+      if (selectedCity) {
+        window.location.href = selectedCity;
+      }
+    });
+  }
 };
-
