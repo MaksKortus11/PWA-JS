01c981261e49d5d6486dd7558c5468bba8f73f34
MaksKortus11 <adiseba11@gmail.com>
2026-01-19 10:42:32 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,5 +4,16 @@ window.onload = () => {
       .then(() => console.log('Service Worker registered successfully.'))
       .catch(err => console.error('Service Worker registration failed:', err));
   }
+  // Obsługa dropdown menu
+const citySelect = document.getElementById('citySelect');
+if (citySelect) {
+  citySelect.addEventListener('change', (e) => {
+    const selectedCity = e.target.value;
+    if (selectedCity) {
+      window.location.href = selectedCity; // przeniesienie na podstronę
+    }
+  });
+}
+
 };
 
