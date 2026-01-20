4f10750c029b4b645e240e9b3c2f21a9d0144c9f
MaksKortus11 <adiseba11@gmail.com>
2026-01-20 10:44:57 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
 const cacheName = 'hoenn-pwa-v1';
+
 const filesToCache = [
   '/',
   '/index.html',
   '/style.css',
   '/js/main.js',
+
+  // pages
   '/cities/dewford.html',
   '/cities/evergrande.html',
   '/cities/fallabor.html',
@@ -20,68 +23,44 @@ const filesToCache = [
   '/cities/slateport.html',
   '/cities/sootopolis.html',
   '/cities/verdanturf.html',
-  '/images/map-hoenn.webp',
-  '/images/cities/dewford.jpg',
-  '/images/cities/evergrande.jpg',
-  '/images/cities/fallabor.jpg',
-  '/images/cities/fortree.jpg',
-  '/images/cities/lavaridge.jpg',
-  '/images/cities/lillycove.jpg',
-  '/images/cities/littleroot.jpg',
-  '/images/cities/mauvile.jpg',
-  '/images/cities/mossdeep.jpg',
-  '/images/cities/oldale.jpg',
-  '/images/cities/pacifidlog.jpg',
-  '/images/cities/petalburg.jpg',
-  '/images/cities/rustboro.jpg',
-  '/images/cities/slateport.jpg',
-  '/images/cities/sootopolis.jpg',
-  '/images/cities/verdanturf.jpg'
+
+  // images
+  '/images/map-hoenn.webp'
 ];
 
-self.addEventListener('install', (event) => {
+// cache install
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
   );
 });
 
-self.addEventListener('fetch', (event) => {
+// fetch handler
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request).then((fetchResponse) => {
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(fetchResponse => {
         if (event.request.method === 'GET') {
-          return caches.open(cacheName).then((cache) => {
+          caches.open(cacheName).then(cache => {
             cache.put(event.request, fetchResponse.clone());
-            return fetchResponse;
           });
         }
         return fetchResponse;
       });
-    }).catch(() => {
-      if (event.request.mode === 'navigate') {
-        return caches.match('/index.html');
-      }
     })
   );
 });
 
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [cacheName];
+// cleanup old cache
+self.addEventListener('activate', event => {
+  const whitelist = [cacheName];
   event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cache) => {
-          if (!cacheWhitelist.includes(cache)) {
-            return caches.delete(cache);
-          }
-        })
-      )
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (!whitelist.includes(key)) {
+          return caches.delete(key);
+        }
+      }))
     )
   );
 });
-
-
-
-
-
-
