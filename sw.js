f2bb5ee7f3c66afb3227b2bb4820a19083800f5e
MaksKortus11 <adiseba11@gmail.com>
2026-01-21 10:46:13 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,30 @@
 const cacheName = 'hoenn-pwa-v1';
+const basePath = '/PWA-JS/';
 
 const filesToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './js/main.js',
-  './cities/dewford.html',
-  './cities/evergrande.html',
-  './cities/fallabor.html',
-  './cities/fortree.html',
-  './cities/lavaridge.html',
-  './cities/lillycove.html',
-  './cities/littleroot.html',
-  './cities/mauvile.html',
-  './cities/mossdeep.html',
-  './cities/oldale.html',
-  './cities/pacifidlog.html',
-  './cities/petalburg.html',
-  './cities/rustboro.html',
-  './cities/slateport.html',
-  './cities/sootopolis.html',
-  './cities/verdanturf.html',
-  './images/map-hoenn-640.webp',
-  './images/map-hoenn-1024.webp',
-  './images/map-hoenn-1350.webp'
+  basePath,
+  basePath + 'index.html',
+  basePath + 'style.css',
+  basePath + 'js/main.js',
+  basePath + 'cities/dewford.html',
+  basePath + 'cities/evergrande.html',
+  basePath + 'cities/fallabor.html',
+  basePath + 'cities/fortree.html',
+  basePath + 'cities/lavaridge.html',
+  basePath + 'cities/lillycove.html',
+  basePath + 'cities/littleroot.html',
+  basePath + 'cities/mauvile.html',
+  basePath + 'cities/mossdeep.html',
+  basePath + 'cities/oldale.html',
+  basePath + 'cities/pacifidlog.html',
+  basePath + 'cities/petalburg.html',
+  basePath + 'cities/rustboro.html',
+  basePath + 'cities/slateport.html',
+  basePath + 'cities/sootopolis.html',
+  basePath + 'cities/verdanturf.html',
+  basePath + 'images/map-hoenn-640.webp',
+  basePath + 'images/map-hoenn-1024.webp',
+  basePath + 'images/map-hoenn-1350.webp'
 ];
 
 self.addEventListener('install', event => {
@@ -33,34 +34,19 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.destination === 'image') {
-    event.respondWith(
-      caches.match(event.request).then(cached => {
-        return cached || fetch(event.request).then(response => {
-          return caches.open(cacheName).then(cache => {
-            cache.put(event.request, response.clone());
-            return response;
-          });
-        });
-      })
-    );
-    return;
-  }
-
   event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        if (event.request.method === 'GET') {
-          caches.open(cacheName).then(cache => cache.put(event.request, response.clone()));
-        }
-        return response;
-      })
-      .catch(() => {
-        if (event.request.mode === 'navigate') {
-          return caches.match('./index.html');
-        }
-        return caches.match(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(fetchResponse => {
+        return caches.open(cacheName).then(cache => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
+    }).catch(() => {
+      if (event.request.mode === 'navigate') {
+        return caches.match(basePath + 'index.html');
+      }
+    })
   );
 });
 
@@ -68,13 +54,9 @@ self.addEventListener('activate', event => {
   const whitelist = [cacheName];
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (!whitelist.includes(key)) {
-            return caches.delete(key);
-          }
-        })
-      )
+      Promise.all(keys.map(key => {
+        if (!whitelist.includes(key)) return caches.delete(key);
+      }))
     )
   );
 });
